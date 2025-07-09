c3aa7573f340e752bec4fd0abb07977d9011fb37
Ragib Gulzar <ragibgulzar@gmail.com>
2025-07-10 00:19:57 +0530

    setup Gradle Java project structure

---
+++ app/src/main/java/org/example/entities/User.java
@@ -0,0 +1,13 @@
+package org.example.entities;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String password;
+    private String hashPassword;
+    private List<Ticket> ticketsBooked;
+    private String userId;
+
+
+}
